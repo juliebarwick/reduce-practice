c3d76d1563eb3998a26942cf60718a2264afec0c
Julie Barwick <julie@juliebarwick.com>
2021-09-12 19:21:21 -0700

    Write filter using reduce

--- reduce.js
+++ reduce.js
@@ -2,3 +2,8 @@
 const map = (arr, fn) => arr.reduce((acc, x) => [...acc, fn(x)], []);
 
 console.log(map([1, 2, 3, 4], num => num * 2));
+
+//Challenge 2: rewrite map with reduce
+const filter = (arr, fn) => arr.reduce((acc, x) => fn(x) ? [...acc, x] : [...acc], []);
+
+console.log(filter([1, 2, 3, 4], num => num % 2 === 0));
