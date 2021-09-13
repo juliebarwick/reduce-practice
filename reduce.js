0bc430b66b3ba60d32628bf6d3b607b0c5ba59bf
Julie Barwick <julie@juliebarwick.com>
2021-09-12 19:46:08 -0700

    Remove console.logs and export functions for testing

--- reduce.js
+++ reduce.js
@@ -1,9 +1,10 @@
 //Challenge 1: rewrite map with reduce
 const map = (arr, fn) => arr.reduce((acc, x) => [...acc, fn(x)], []);
 
-console.log(map([1, 2, 3, 4], num => num * 2));
-
 //Challenge 2: rewrite map with reduce
 const filter = (arr, fn) => arr.reduce((acc, x) => fn(x) ? [...acc, x] : [...acc], []);
 
-console.log(filter([1, 2, 3, 4], num => num % 2 === 0));
+module.exports = {
+  map,
+  filter,
+};
