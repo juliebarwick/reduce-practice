64c91eb99b759966063ccf568a49d75f21b3c3c7
Julie Barwick <julie@juliebarwick.com>
2021-09-14 15:01:29 -0700

    Implement flatten using recursion and reduce

--- reduce.js
+++ reduce.js
@@ -7,8 +7,18 @@ const filter = (arr, fn) => arr.reduce((acc, x) => fn(x) ? [...acc, x] : [...acc
 // Challenge 3: write a function that returns the average of an array of values using reduce
 const average = (arr) => arr.reduce((acc, x) => acc + x) / arr.length;
 
+// Challenge 4: flatten a nested array
+const flatten = (nestedArray, initial = []) => nestedArray.reduce((acc, x) => {
+  if (Array.isArray(x)) {
+    return flatten(x, acc);
+  } else {
+    return [...acc, x];
+  }
+}, initial);
+
 module.exports = {
   map,
   filter,
-  average
+  average,
+  flatten,
 };
