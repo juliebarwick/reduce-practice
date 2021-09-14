4fe9fceb859d5f473172a445bcf0a23f0002e472
Julie Barwick <julie@juliebarwick.com>
2021-09-14 15:01:02 -0700

    Edit challenge 3 description in comment

--- reduce.js
+++ reduce.js
@@ -4,8 +4,7 @@ const map = (arr, fn) => arr.reduce((acc, x) => [...acc, fn(x)], []);
 //Challenge 2: rewrite map with reduce
 const filter = (arr, fn) => arr.reduce((acc, x) => fn(x) ? [...acc, x] : [...acc], []);
 
-// Challenge 3: write a function that returns the average of an array
-// using reduce
+// Challenge 3: write a function that returns the average of an array of values using reduce
 const average = (arr) => arr.reduce((acc, x) => acc + x) / arr.length;
 
 module.exports = {
